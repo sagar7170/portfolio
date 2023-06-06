319877db23214e8570f3e9f8cffcf298f9a9b06a
Sagar <sagarkarotia@gmail.com>
2023-06-06 17:28:01 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
+import {FcDownload} from 'react-icons/fc'
+function Header() {
+  return (
+    <header className='sticky top-0 flex items-start p-5 justify-between max-w-7xl mx-auto z-20 xl:items-center '>
+    
+    {/* social icons */}
+    
+  <motion.div 
+    initial = {{
+        x: -500,
+        opacity:0,
+        scale:0.5
+      }}
+    animate={{
+      x:0,
+      opacity:1,
+      scale:1
+    }}
+    transition={{
+      duration:1.5
+    }}
+    className='flex flex-row items-center '>
+      <SocialIcon url="https://twitter.com/sagarkarotia" 
+      fgColor='gray' 
+      bgColor='transparent'/>
+      {/* <SocialIcon url="https://www.instagram.com/sagarkarotia/" 
+      fgColor='gray'   
+      bgColor='transparent'/> */}
+      <SocialIcon url="https://github.com/sagar7170" 
+      fgColor='gray'
+      bgColor='transparent'/>
+      <SocialIcon url="https://www.linkedin.com/in/sagar-karotia-69132a214/" 
+      fgColor='gray'
+      bgColor='transparent'/>
+     
+  </motion.div>
+  
+{/* email icon */}
+
+  <motion.div 
+     initial = {{
+      x: 500,
+      opacity:0,
+      scale:0.5
+    }}
+  animate={{
+    x:0,
+    opacity:1,
+    scale:1
+  }}
+  transition={{
+    duration:1.5
+  }}
+    className='flex flex-row items-center space-x-2'>    
+      <a href="https://drive.google.com/file/d/1bhw2rH6E9qs3PCmn1nYcaVgoDYKL9KsM/view?usp=sharing"> 
+      <FcDownload  className='rounded-md h-6 w-9 animate-bounce'/></a>
+     <a href="#mail"> <SocialIcon 
+      className='curser-pointer'
+      network='email'
+      fgColor='gray'
+      bgColor='transparent'
+      />
+      <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
+      </a>
+      
+    </motion.div>
+    </header>
+  )
+}
+export default Header
